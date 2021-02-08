6ff796cd645207c850f302b0807905fc2ea4ef28
Greg Parker <gregjp91@gmail.com>
2021-02-08 03:07:51 -0500

    Finished first 2 parts of challenge

---
+++ queries.sql
@@ -0,0 +1,47 @@
+create table departments (
+	dept_no varchar primary key,
+	dept_name varchar);
+	
+create table titles (
+	title_id varchar primary key,
+	title varchar);
+
+create table employees (
+	emp_no int primary key,
+	emp_title_id varchar,
+	birth_date date,
+	first_name varchar,
+	last_name varchar,
+	sex varchar,
+	hire_date date,
+	foreign key (emp_title_id) references titles(title_id));
+	
+create table dept_emp (
+	emp_no int,
+	dept_no varchar,
+	foreign key (emp_no) references employees(emp_no),
+	foreign key (dept_no) references departments(dept_no));
+	
+create table dept_manager (
+	dept_no varchar,
+	emp_no int,
+	foreign key (dept_no) references departments(dept_no),
+	foreign key (emp_no) references employees(emp_no));
+	
+create table salaries (
+	emp_no int,
+	salary int,
+	foreign key (emp_no) references employees(emp_no));
+	
+-- Challenge 1
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employees e
+join salaries s
+on (e.emp_no = s.emp_no);
+
+-- Challenge 2
+select first_name, last_name, hire_date
+from employees
+where hire_date between '1986-01-01' and '1986-12-31';
+
+-- Challenge 3
